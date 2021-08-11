3f245c4de874d7e202466e33996cb4ca762e5b8e
Peter Sidra <peter.sidra@gmail.com>
2021-08-12 00:01:45 +0200

    - Refactored the code to make it more extensible - Added extra units

--- lib/responsive_units.dart
+++ lib/responsive_units.dart
@@ -5,10 +5,28 @@ import 'dart:math';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 
-/// A utility class acting as a namespace for the global
-/// [_ResponsiveUnitsUtil.size] variable
+/// A utility class that abstracts media queries
 class _ResponsiveUnitsUtil {
-  static Size size = const Size(0, 0);
+  static MediaQueryData mediaQueryData = const MediaQueryData();
+
+  static double get width => mediaQueryData.size.width;
+
+  static double get height => mediaQueryData.size.height;
+
+  static double get diagonal => sqrt(width * width + height * height);
+
+  static double get safeWidth =>
+      mediaQueryData.size.width -
+      (mediaQueryData.padding.left + mediaQueryData.padding.right);
+
+  static double get safeHeight =>
+      mediaQueryData.size.height -
+      (mediaQueryData.padding.top + mediaQueryData.padding.bottom);
+
+  static double get safeDiagonal =>
+      sqrt(safeWidth * safeWidth + safeHeight * safeHeight);
+
+  static double get textScaleFactor => mediaQueryData.textScaleFactor;
 }
 
 /// This widget is intended to be inserted above the Navigator of
@@ -26,13 +44,13 @@ class AppSizer extends StatelessWidget {
   /// The child widget that AppSizer will display
   final Widget? child;
 
-  /// This flag infroms [AppSizer] whether it should force a rebuild
-  /// of the widget when the app's size changes
+  /// This flag informs [AppSizer] whether it should force a rebuild
+  /// of the widget when the app's media query data changes
   ///
   /// If this flag is set to false, widgets that don't depend on media
   /// query in their build methods might not rebuild on size change events.
-  /// However, calling the extension methods on num [.h, .w, .dg]
-  /// would still return correct results
+  /// However, calling the extension methods on num [.h, .w, .dg, etc.]
+  /// would still return correct values
   final bool rebuildTree;
 
   const AppSizer({Key? key, this.child, this.rebuildTree = true})
@@ -40,13 +58,13 @@ class AppSizer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final Size size = MediaQuery.of(context).size;
+    final mediaQueryData = MediaQuery.of(context);
 
-    if (rebuildTree && size != _ResponsiveUnitsUtil.size) {
+    if (rebuildTree && mediaQueryData != _ResponsiveUnitsUtil.mediaQueryData) {
       _rebuildAllChildren(context);
     }
 
-    _ResponsiveUnitsUtil.size = size;
+    _ResponsiveUnitsUtil.mediaQueryData = mediaQueryData;
 
     return child ?? const SizedBox();
   }
@@ -56,30 +74,53 @@ extension ResponsiveUnitsX on num {
   /// Returns height in pixels calculated as a percentage of the
   /// screen's height
   ///
-  /// ex: ```50.h``` returns 50% of the screens height
-  double get h => this / 100 * _ResponsiveUnitsUtil.size.height;
+  /// ex: ```50.h``` returns 50% of the screen's height
+  double get h => this / 100 * _ResponsiveUnitsUtil.height;
 
   /// Returns width in pixels calculated as a percentage of the
   /// screen's width
   ///
-  /// ex: ```50.w``` returns 50% of the screens width
-  double get w => this / 100 * _ResponsiveUnitsUtil.size.width;
+  /// ex: ```50.w``` returns 50% of the screen's width
+  double get w => this / 100 * _ResponsiveUnitsUtil.width;
 
-  /// Returns diagonal lentgh in pixels calculated as a percentage of the
+  /// Returns diagonal length in pixels calculated as a percentage of the
   /// screen's diagonal length
   ///
-  /// ex: ```50.dg``` returns 50% of the screens diagonal
+  /// ex: ```50.dg``` returns 50% of the screen's diagonal
   ///
   /// This is useful for scaling text and corners
-  double get dg =>
-      this /
-      100 *
-      sqrt(_ResponsiveUnitsUtil.size.width * _ResponsiveUnitsUtil.size.width +
-          _ResponsiveUnitsUtil.size.height * _ResponsiveUnitsUtil.size.height);
+  double get dg => this / 100 * _ResponsiveUnitsUtil.diagonal;
+
+  /// Returns the (safe) height in pixels calculated as a percentage of the
+  /// screen's height excluding the parts of the display that are partially
+  /// obscured by system UI (such as notches and the status bar)
+  ///
+  /// ex: ```50.sfh``` returns 50% of the screen's safe height
+  double get sfh => this / 100 * _ResponsiveUnitsUtil.safeHeight;
+
+  /// Returns the (safe) width in pixels calculated as a percentage of the
+  /// screen's width excluding the parts of the display that are partially
+  /// obscured by system UI (such as notches and the status bar)
+  ///
+  /// ex: ```50.sfw``` returns 50% of the screen's safe width
+  double get sfw => this / 100 * _ResponsiveUnitsUtil.safeWidth;
+
+  /// Returns the (safe) diagonal length in pixels calculated as a percentage
+  /// of the screen's diagonal length excluding the parts of the display that
+  /// are partially obscured by system UI (such as notches and the status bar)
+  ///
+  /// ex: ```50.sfdg``` returns 50% of the screen's safe diagonal
+  ///
+  /// This is useful for scaling text and corners
+  double get sfdg => this / 100 * _ResponsiveUnitsUtil.safeDiagonal;
+
+  /// Returns a scalable pixel value that scales with the user selected text
+  /// scale factor
+  double get sp => this * _ResponsiveUnitsUtil.textScaleFactor;
 }
 
 // https://stackoverflow.com/a/58513635
-/// Rebuilds the enitre widget tree below context
+/// Rebuilds the entire widget tree below context
 void _rebuildAllChildren(BuildContext context) {
   void rebuild(Element el) {
     el.markNeedsBuild();
